f54308c40c5f46a089901841c9a60fffda33ce88
Jason Reed <jcreed@gmail.com>
2024-10-22 10:56:12 -0400

    This works with all three switches writing to serial

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
+# Should be done with original firmware
 all:
 	arduino-cli compile --fqbn arduino:avr:uno src
 	arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno src
+
+# Flash original arduino firmware
+flash-original:
+	dfu-programmer atmega16u2 erase
+	dfu-programmer atmega16u2 flash firmware/existing-usbserial.hex
+	dfu-programmer atmega16u2 reset
+
+# Flash HID device firmware
+flash-keyboard:
+	dfu-programmer atmega16u2 erase
+	dfu-programmer atmega16u2 flash Arduino-keyboard-0.3.hex
+	dfu-programmer atmega16u2 reset
--- src/stompboard.h
+++ src/stompboard.h
@@ -1,33 +1,58 @@
 uint8_t keyNone[8] = { 0, 0, 0, 0, 0, 0, 0 };
 uint8_t keySpace[8] = { 0, 0, 44, 0, 0, 0, 0 };
 
-int readPin = 8;
+int switch1 = 5;
+int switch2 = 4;
+int switch3 = 3;
 int onboardPin = 13;
 
 void setup() {
-  pinMode(readPin, INPUT);
+  pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
+  pinMode(switch3, INPUT);
+
   pinMode(onboardPin, OUTPUT);
-  Serial.begin(9600);
+    Serial.begin(9600);
+}
+
+int ledVal = HIGH;
+
+       /* Serial.write(keySpace, 8); */
+       /* Serial.read(); */
+       /* Serial.write(keyNone, 8); */
+
+void handleSwitch(int index) {
+  ledVal = !ledVal;
+  digitalWrite(onboardPin, ledVal);
+  char buf[64];
+  snprintf(buf, 63, "switch %d\n", index);
+  Serial.write(buf, strlen(buf));
 }
 
 void loop() {
-  int ledVal = HIGH;
   digitalWrite(onboardPin, ledVal);
-  int oldVal = digitalRead(readPin);
+  int switch1val = digitalRead(switch1);
+  int switch2val = digitalRead(switch2);
+  int switch3val = digitalRead(switch3);
 
   while (1) {
-    int newVal = digitalRead(readPin);
-    if (oldVal != newVal) {
-
-      // Send space character
-      Serial.write(keySpace, 8);
-      Serial.read();
-      Serial.write(keyNone, 8);
-
-      ledVal = !ledVal;
-      digitalWrite(onboardPin, ledVal);
-      delay(100);
-      oldVal = newVal;
+    int new1val = digitalRead(switch1);
+    int new2val = digitalRead(switch2);
+    int new3val = digitalRead(switch3);
+    if (switch1val != new1val) {
+      handleSwitch(1);
+      switch1val = new1val;
+      delay(500);
+    }
+    if (switch2val != new2val) {
+      handleSwitch(2);
+      switch2val = new2val;
+      delay(500);
+    }
+    if (switch3val != new3val) {
+      handleSwitch(3);
+      switch3val = new3val;
+      delay(500);
     }
   }
 }
